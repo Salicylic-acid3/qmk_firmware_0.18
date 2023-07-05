a5d6985756cf3ff90690d7f5fff0ede2930b8ebc
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2023-07-06 02:40:33 +0900

    修正

--- keyboards/yushakobo/ergo68/keymaps/default/keymap.c
+++ keyboards/yushakobo/ergo68/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 Salicylic_acid3
+/* Copyright 2023 yushakobo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
